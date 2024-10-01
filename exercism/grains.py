0500566514fab19e19bbbc9232e163318facc370
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-01 16:41:27 -0400

    Delete exercism/grains.py

--- exercism/grains.py
+++
@@ -1,8 +0,0 @@
-def square(number):
-    if number < 1 or number > 64:
-        return ValueError("Square must be between 1 and 64")
-    return 2 ** (number - 1)
-
-
-def total():
-    return (2**64) - 1
