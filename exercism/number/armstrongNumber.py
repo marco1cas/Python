18f2b39b7ea876c98f3914621434570c6936d100
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-04 16:36:47 -0400

    Update armstrongNumber.py

--- exercism/number/armstrongNumber.py
+++ exercism/number/armstrongNumber.py
@@ -3,3 +3,8 @@ def is_armstrong_number(number):
     dig = len(s)
     form = sum(int(d) ** dig for d in s)
     return form == number
+
+#Second Version
+def is_armstrong_number2(number):
+    return number = sum(int(var) ** len(str(number)) for var in str(number))
+    
