8597c57bb8977e2cf8d781e9fa2c891ce10d623a
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-07 16:24:38 -0400

    Update leap.py

--- exercism/bool/leap.py
+++ exercism/bool/leap.py
@@ -3,9 +3,11 @@ def leap_year(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+            return False
+        return True
+     return False
+
+
+#Second Version
+def leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 ==0)
