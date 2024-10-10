4ce6ea61607a4a4c4893a0bf7d6a50e91fba01f8
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-10 16:45:15 -0400

    Update matchingBrackets.py

--- exercism/conditionals/matchingBrackets.py
+++ exercism/conditionals/matchingBrackets.py
@@ -9,3 +9,17 @@ def is_paired(input_string):
             if not arr or abierto[cerrado.index(i)] != arr.pop():
                 return False
     return not arr
+
+#Second Version
+def is_paired2(input_string):
+    abierto = "[{("
+    cerrado = "]})"
+    pairs = {')': '(', '}':'{', ']':'['}
+    arr = []
+    for i in input_string:
+        if i in abierto:
+            arr.append(i)
+        elif i in cerrado:
+            if not arr or arr.pop() != pairs[i]:
+                return False
+    return not arr
