6b790b4967f6c7a543e350594291362bce37eec3
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-11 16:56:46 -0400

    Update littleSister'sVocabulary.py

--- exercism/strings/littleSister'sVocabulary.py
+++ exercism/strings/littleSister'sVocabulary.py
@@ -16,8 +16,6 @@ def remove_suffix_ness(word):
     else:
         return new_word
 
-
-
 def adjective_to_verb(sentence, index):
     adjective = sentence.split()[index]
     if adjective[-1] == "." or adjective[-1] == "!" or adjective[-1] == ",":
@@ -26,4 +24,28 @@ def adjective_to_verb(sentence, index):
     else:
         verb = adjective + "en"
     return verb
+
+
+# Second Version
+def add_prefix_un(word):
+    return "un" + word
+
+
+def make_word_groups(vocab_words):
+    prefix = vocab_words[0]
+    return prefix + " :: " + " :: ".join([prefix + i for i in vocab_words[1:]])
+
+
+def remove_suffix_ness(word):
+    new_word = word[:-4]
+    if new_word.endswith("i"):
+        new_word = new_word[:-1]
+        new_word = new_word + "y"
+        return new_word
+    else:
+        return new_word
+
+def adjective_to_verb(sentence, index):
+     adjective = sentence.split()[index].strip(".,!")
+    return adjective + "en"
 
