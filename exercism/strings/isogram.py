1ba1349ea62b81dc81fcb9a42776de379c6dc58e
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-10-14 10:03:25 -0400

    Update isogram.py

--- exercism/strings/isogram.py
+++ exercism/strings/isogram.py
@@ -1,3 +1,8 @@
 def is_isogram(string):
     string = string.lower().replace(" ", "").replace("-", "")
     return len(string) == len(set(string))
+
+#Second Version
+def is_isogram2(string):
+    letters = [i for i in string.lower() if i.isalpha()]
+    return len(letters) == len(set(letters))
